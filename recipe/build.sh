ec38a7a248ddaf3af0d2d3d8dbcc0df13a0b8b8f
Min RK <benjaminrk@gmail.com>
2016-10-28 14:39:37 +0200

    move tests to run_test.sh
    from build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,13 +13,6 @@ else
   PREFIX2="build/Linux-x86_64"
 fi
 
-$PREFIX2/programs/mpmetis graphs/metis.mesh 10
-$PREFIX2/programs/gpmetis graphs/mdual.graph 10
-$PREFIX2/programs/ndmetis graphs/mdual.graph 10
-$PREFIX2/programs/gpmetis graphs/test.mgraph 10
-$PREFIX2/programs/m2gmetis graphs/metis.mesh 10
-
-cp $PREFIX2/libmetis/libmetis.a $PREFIX/lib/
 cp $PREFIX2/programs/cmpfillin $PREFIX/bin/
 cp $PREFIX2/programs/gpmetis $PREFIX/bin/
 cp $PREFIX2/programs/graphchk $PREFIX/bin/
