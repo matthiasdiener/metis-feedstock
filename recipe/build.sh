3d6fcada961017dc2a1749b289c6cd19cdc20f94
Min RK <benjaminrk@gmail.com>
2016-10-28 14:43:22 +0200

    run make install
    instead of manually copying files, which was missing some things (gklib_tls.h)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-make config
+make config \
+     shared=1 \
+     prefix=$PREFIX
+
 make
-# install.
-mkdir -p $PREFIX/lib/
-mkdir -p $PREFIX/bin/
-mkdir -p $PREFIX/include/
-
-if [ `uname` == "Darwin" ]; then
-  PREFIX2="build/Darwin-x86_64"
-else
-  PREFIX2="build/Linux-x86_64"
-fi
-
-cp $PREFIX2/programs/cmpfillin $PREFIX/bin/
-cp $PREFIX2/programs/gpmetis $PREFIX/bin/
-cp $PREFIX2/programs/graphchk $PREFIX/bin/
-cp $PREFIX2/programs/m2gmetis $PREFIX/bin/
-cp $PREFIX2/programs/ndmetis $PREFIX/bin/
-cp $PREFIX2/programs/mpmetis $PREFIX/bin/
-cp include/metis.h $PREFIX/include/
+make install
