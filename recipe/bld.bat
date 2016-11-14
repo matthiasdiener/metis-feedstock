850e5102d58c963d757741c07042de45cf1473a7
Bas Nijholt <basnijholt@gmail.com>
2016-11-14 15:52:02 +0100

    enable shared libs

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,11 +5,8 @@ cmake ^
     -G "%CMAKE_GENERATOR%" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -DBUILD_STATIC_LIBS=1 ^
-    -DBUILD_SHARED_LIBS=1 ^
+    -DBUILD_SHARED_LIBS=yes ^
     ..\..
-
-ECHO VS files have been generated in build\windows
 
 cmake --build . --config Release --target ALL_BUILD
 cmake --build . --config Release --target ZERO_CHECK
