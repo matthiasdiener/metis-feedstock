dafd3c2b6fec044ff6811aea45193a23fb0545f6
Bas Nijholt <basnijholt@gmail.com>
2016-11-14 16:31:43 +0100

    try again to use NMake Makefiles

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ MKDIR build\windows
 CD build\windows
 
 cmake ^
-    -G "%CMAKE_GENERATOR%" ^
+    -G "NMake Makefiles"                     ^
 	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
 	-DCMAKE_INSTALL_BINDIR=%LIBRARY_BIN%     ^
 	-DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB%     ^
