53f7cd3e7a9022a8781b8f3a786bde676b3f16ae
Bas Nijholt <basnijholt@gmail.com>
2016-11-14 15:45:45 +0100

    Cleanup bat file

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,13 @@
 MKDIR build\windows
 CD build\windows
 
-cmake ..\.. ^
-        -G "%CMAKE_GENERATOR%" ^
-        -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-        -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-        -DBUILD_STATIC_LIBS=1 ^
-        -DBUILD_SHARED_LIBS=1
+cmake ^
+    -G "%CMAKE_GENERATOR%" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DBUILD_STATIC_LIBS=1 ^
+    -DBUILD_SHARED_LIBS=1 ^
+    ..\..
 
 ECHO VS files have been generated in build\windows
 
