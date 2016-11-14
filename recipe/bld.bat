e2af1311376bce5928d9f0fb7693377857e17038
Bas Nijholt <basnijholt@gmail.com>
2016-11-14 14:41:15 +0100

    add cmake config

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,13 @@
 MKDIR build\windows
 CD build\windows
-cmake -DCMAKE_CONFIGURATION_TYPES="Release" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..\..
+
+cmake ..\.. ^
+        -G %CMAKE_GENERATOR% ^
+        -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+        -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+        -DBUILD_STATIC_LIBS=1 ^
+        -DBUILD_SHARED_LIBS=1
+
 ECHO VS files have been generated in build\windows
-cmake --build .
+
+cmake --build . --config Release
