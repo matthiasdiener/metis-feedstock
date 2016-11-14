7918b541e73fbfd569fa1fdebad35e858e6bcbfa
Bas Nijholt <basnijholt@gmail.com>
2016-11-14 14:07:13 +0100

    Enable Windows building

---
+++ recipe/bld.bat
@@ -0,0 +1,5 @@
+MKDIR build\windows
+CD build\windows
+cmake -DCMAKE_CONFIGURATION_TYPES="Release" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..\..
+ECHO VS files have been generated in build\windows
+CD ..\..\
