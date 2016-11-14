d1797ec8ff72d41e91825bbbc7efaa91cd8ae475
Bas Nijholt <basnijholt@gmail.com>
2016-11-14 14:14:16 +0100

    add CMAKE_GENERATOR

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 MKDIR build\windows
 CD build\windows
-cmake -DCMAKE_CONFIGURATION_TYPES="Release" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..\..
+cmake -DCMAKE_CONFIGURATION_TYPES="Release" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..\.. "%CMAKE_GENERATOR%"
 ECHO VS files have been generated in build\windows
 CD ..\..\
