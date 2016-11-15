fbbde85d7355303870d1bbd43455257860f18af4
Bas Nijholt <basnijholt@gmail.com>
2016-11-15 11:43:24 +0100

    correct locations of exe files

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -18,14 +18,11 @@ dir libmetis
 dir programs
 dir include
 dir Release
-dir libmetis\Release
-dir programs\Release
-dir include\Release
 
-copy libmetis\Release\metis.lib %LIBRARY_BIN%
-copy programs\Release\cmpfillin.exe %LIBRARY_LIB%
-copy programs\Release\gpmetis.exe %LIBRARY_LIB%
-copy programs\Release\graphchk.exe %LIBRARY_LIB%
-copy programs\Release\m2gmetis.exe %LIBRARY_LIB%
-copy programs\Release\ndmetis.exe %LIBRARY_LIB%
-copy programs\Release\mpmetis.exe %LIBRARY_LIB%
+copy libmetis\metis.lib %LIBRARY_BIN%
+copy programs\cmpfillin.exe %LIBRARY_LIB%
+copy programs\gpmetis.exe %LIBRARY_LIB%
+copy programs\graphchk.exe %LIBRARY_LIB%
+copy programs\m2gmetis.exe %LIBRARY_LIB%
+copy programs\ndmetis.exe %LIBRARY_LIB%
+copy programs\mpmetis.exe %LIBRARY_LIB%
